2a0c5630ff873ce792e619cd4266ea4149a8a039
Sumit Jindal <sumitjindal66@gmail.com>
2021-01-06 03:48:31 -0500

    Refactor card component button functionality add

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,6 +1,23 @@
 import './card.css'
 import dayjs from 'dayjs';
+import { useContext } from 'react';
+import { GlobalContext } from '../../context/GlobalState';
+
+
 const Card = (props) => {
+    const {
+        addMovieToWatchlist,
+        addMovieToWatched,
+        watchlist,
+        watched
+    } = useContext(GlobalContext);
+
+    let storedMovie = watchlist.find((res)=> res.id === props.movie.id);
+    let watchedMovie = watched.find((res)=> res.id === props.movie.id);
+
+    const watchlistDisabled = storedMovie ? true : watchedMovie ? true : false;
+    const watchedDisabled = watchedMovie ? true : false;
+
     return (
         <div className="card">
             <div className="card__img">
@@ -14,10 +31,16 @@ const Card = (props) => {
                     <h4 className="card__date">{ dayjs(props.movie.release_date).format('YYYY')}</h4>
                 </div>
                 <div className="card__buttons">
-                    <button className="btn">
+                    <button 
+                        className="btn"
+                        disabled={watchlistDisabled}
+                        onClick={()=> addMovieToWatchlist(props.movie)}>
                         Add to Watchlist
                     </button>
-                    <button className="btn">
+                    <button 
+                        className="btn"
+                        disabled={watchedDisabled}
+                        onClick={()=>addMovieToWatched(props.movie)}>
                         Add to Watched
                     </button>
                 </div>
