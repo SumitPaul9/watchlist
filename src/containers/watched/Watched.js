4105ef562d08755df65748e5e3cefc0981b5e611
Sumit Jindal <sumitjindal66@gmail.com>
2021-01-06 22:33:29 -0500

    Add watched container

---
+++ src/containers/watched/Watched.js
@@ -0,0 +1,25 @@
+import { useContext } from "react"
+import WatchCard from "../../components/watchcard/WatchCard";
+import { GlobalContext } from "../../context/GlobalState"
+
+const Watched = () => {
+    const {watched} = useContext(GlobalContext);
+    return (
+        <div className="watched container movie__page">
+            <div className="watched__header">
+                <h1 className="watched__heading heading">Watched Movies</h1>
+            </div>
+            { watched.length > 0 ? (
+                <div className="movie__grid">
+                    { watched.map((movie)=>(
+                     <WatchCard movie={movie} key={movie.id}/>   
+                    ))}
+                </div>
+            ): (
+                <h2>You have nothing in your watchlist! Please Add some!</h2>
+            )}
+        </div>
+    )
+}
+
+export default Watched
